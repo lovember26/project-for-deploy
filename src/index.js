07a0a18ac3d7846e530e5e7452061edbe6a99910
Tetiana Prokopchuk <tetyanaprokopchuk@ukr.net>
2023-08-15 12:27:59 +0300

    edit

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import reportWebVitals from "reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="buzzTalk-chat-front-end">
+    <BrowserRouter>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistedStore}>
           <ThemeProvider theme={theme}>
